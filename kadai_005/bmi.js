0fa660018ffdff9842f4497b813a8cc7f1eab950
youtomo366 <keisukekeisuke426@gmail.com>
2025-06-29 22:36:57 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+let weight; // 体重 (kg)
+let height; // 身長 (m)
+let bmi;    // BMIを保存するための変数
+
+weight = 68;
+height = 1.7;
+
+// BMIを計算し、変数に代入します
+bmi = weight / (height * height);
+
+// BMIの結果を表示します
+console.log(bmi);
